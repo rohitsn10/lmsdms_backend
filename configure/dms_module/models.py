95c311c8587ed13cdfbf5fa69c87f347af06766f
kalpeshn10 <kalpesh.g@n10tech.com>
2024-10-18 10:00:04 +0530

    add document type module

--- configure/dms_module/models.py
+++ configure/dms_module/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user_profile.models import *
 
 class WorkFlowModel(models.Model):
     workflow_name = models.TextField(blank=True, null=True)
@@ -8,3 +9,10 @@ class WorkFlowModel(models.Model):
 
     def __str__(self):
         return self.workflow_name
+    
+
+class DocumentType(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    document_name = models.TextField(blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- configure/dms_module/urls.py
+++ configure/dms_module/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
    path('create_get_workflow', WorkFlowViewSet.as_view({'post': 'create','get':'list'}),name='create_get_workflow'),
    path('update_delete_workflow/<int:workflow_id>',WorkFlowUpdateSet.as_view({'put': 'update','delete':'destroy'}),name='update_delete_workflow'),
-      
+   
+   path('create_get_department', DocumentTypeCreateViewSet.as_view({'post': 'create','get':'list'}),name='create_get_department'),
 
 ]
