6b31ac452dc7ee5a4c03b95e3552f6ec5c71072c
kalpeshn10 <kalpesh.g@n10tech.com>
2024-11-28 18:08:00 +0530

    add version control add and update code

--- configure/dms_module/urls.py
+++ configure/dms_module/urls.py
@@ -68,6 +68,4 @@ urlpatterns = [
    path('create_comment', DocumentCommentCreateViewSet.as_view({'post': 'create'}), name='create_comment'),
    path('view_comment', DocumentCommentsViewSet.as_view({'get': 'list'}), name='view_comment'),
    path('delete_comment/<comment_id>', DocumentCommentDeleteViewSet.as_view({'delete': 'delete_comment'}), name='DeleteInventory'),
-
-
 ]
