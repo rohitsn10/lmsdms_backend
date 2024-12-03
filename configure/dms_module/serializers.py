a7239011a2d19c6365430fa28551641493a937af
vasuN10 <vasu.p@n10tech.com>
2024-12-03 09:27:12 +0530

    date change

--- configure/dms_module/serializers.py
+++ configure/dms_module/serializers.py
@@ -73,18 +73,18 @@ class CustomUserdataSerializer(serializers.ModelSerializer):
 
 class DocumentviewSerializer(serializers.ModelSerializer):
     document_type_name = serializers.SerializerMethodField()
-    formatted_created_at = serializers.SerializerMethodField()
+    # formatted_created_at = serializers.SerializerMethodField()
     current_status_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Document
-        fields = ['id', 'document_title','assigned_to', 'document_number', 'formatted_created_at', 'document_type_name','form_status','document_current_status','current_status_name','version','training_required']
+        fields = ['id', 'document_title','assigned_to', 'document_number', 'created_at', 'document_type_name','form_status','document_current_status','current_status_name','version','training_required']
 
     def get_document_type_name(self, obj):
         return obj.document_type.document_name if obj.document_type else None
 
-    def get_formatted_created_at(self, obj):
-        return obj.created_at.strftime('%d-%m-%Y')
+    # def get_formatted_created_at(self, obj):
+    #     return obj.created_at.strftime('%d-%m-%Y')
     
     def get_current_status_name(self, obj):
         return obj.document_current_status.status if obj.document_current_status else None
