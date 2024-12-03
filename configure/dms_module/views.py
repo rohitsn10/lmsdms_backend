9887802387cf81788a67579d60dc136b6c98e89f
vasuN10 <vasu.p@n10tech.com>
2024-12-03 16:52:44 +0530

    changes

--- configure/dms_module/views.py
+++ configure/dms_module/views.py
@@ -1685,12 +1685,12 @@ class DocumentTypeUpdateViewSet(viewsets.ModelViewSet):
 
 class DepartmentUsersViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
-    lookup_field = 'id'
+    lookup_field = 'document_id'
 
     def list(self, request, *args, **kwargs):
         try:
             # Get the document ID from the request
-            document_id = self.kwargs.get('document_id', None)
+            document_id = self.kwargs.get('document_id')
             if not document_id:
                 return Response({
                     "status": False,
