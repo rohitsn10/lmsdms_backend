426551313f6da58cf2049c4195b3b0fcd7ac15a1
Parth Darji <interns@n10tech.com>
2025-01-23 13:12:25 +0530

    classroom_id

--- configure/lms_module/serializers.py
+++ configure/lms_module/serializers.py
@@ -254,9 +254,10 @@ class ClassroomTrainingFileSerializer(serializers.ModelSerializer):
 
 class ClassroomTrainingSerializer(serializers.ModelSerializer):
     files = ClassroomTrainingFileSerializer(many=True, read_only=True)
+    classroom_id = serializers.IntegerField(source='id')
     class Meta:
         model = ClassroomTraining
-        fields = ['classroom_name', 'is_assesment', 'description', 'status', 'files', 'created_at']
+        fields = ['classroom_id', 'classroom_name', 'is_assesment', 'description', 'status', 'files', 'created_at']
     # department_of_employee_first_name  = serializers.ReadOnlyField(source='department_or_employee.first_name')
     # department_of_employee_last_name = serializers.ReadOnlyField(source='department_or_employee.last_name')
     # class Meta:
