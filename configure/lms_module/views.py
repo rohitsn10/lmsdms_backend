4256ff612ee24babf4197d3abebacddf8b2610ef
internsn10 <interns@n10tech.com>
2025-03-28 02:25:15 +0530

    quiz reandom update

--- configure/lms_module/views.py
+++ configure/lms_module/views.py
@@ -2011,7 +2011,8 @@ class TrainingQuizUpdateView(viewsets.ModelViewSet):
 
                         questions_list = list(questions)
                         print(questions_list, "jjj")
-                        selected_questions = random.sample(questions_list, count)
+                        # selected_questions = random.sample(questions_list, count)
+                        selected_questions = questions_list
 
                         potential_marks = total_marks_accumulated + (marks * count)
                         if potential_marks > total_marks:
